cff7d5305f2093ac6406af82a59a9380068b5a0f
Pietro-03 <lkj774529@gmail.com>
2025-02-24 11:20:54 -0300

    adicionado novas coisas

--- tarefas/lib/widgets/todo_list_item.dart
+++ tarefas/lib/widgets/todo_list_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:intl/intl.dart';
 import 'package:tarefas/models/todo.dart';
 
 class TodoListItem extends StatelessWidget {
@@ -17,7 +20,52 @@ class TodoListItem extends StatelessWidget {
         vertical: 2,
       ),
       child: Slidable(
-        child: Container(),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: Colors.grey[200],
+          ),
+          padding: const EdgeInsets.all(10),
+          height: 60,
+
+
+          //conteúdo da tarefa
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // exibe a data e hora
+              Text(
+                DateFormat('dd/mm/yyy - HH:mm').format(todo.dateTime),
+                style: TextStyle(fontSize: 12),
+              ),
+            // exibe o titulo da tarefa
+            Text(
+              todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              )
+            )
+
+            ],
+          ),
+        ),
+
+        //definindo a ação do botão de exclusão
+        endActionPane: ActionPane(
+          motion: const DrawerMotion(),
+          extentRatio: 0.25,
+          children: [
+            SlidableAction(
+              label: 'deletar',
+              backgroundColor: Colors.red,
+              icon: Icons.delete,
+              onPressed: (context) {
+                onDelete(todo);
+              },
+            ),
+          ],
+          ),
       ),
     );
   }
